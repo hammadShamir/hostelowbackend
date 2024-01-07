f8ed34358d4fc58072ac34de68e869558aea2e49
hammadShamir <hammadshamir7@gmail.com>
2024-01-07 15:08:51 +0500

    Increased Request Limit

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const AmenitiesRouter = require("./src/router/amentities_route");
 const RoomsRouter = require("./src/router/room_route");
 
 // Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cors());
 app.use(morgan('dev'));
 
