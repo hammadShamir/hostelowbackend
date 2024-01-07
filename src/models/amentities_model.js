affd3b0ac7e12d51a7a902906336fcebb646d9ae
rashidjatoi <rashidjatoi31@gmail.com>
2024-01-07 19:06:12 +0500

    Amentities changes

--- src/models/amentities_model.js
+++ src/models/amentities_model.js
@@ -14,7 +14,7 @@ const AmentittiesSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-  freeParking: {
+  bikeParking: {
     type: Boolean,
     default: false,
   },
@@ -30,10 +30,71 @@ const AmentittiesSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-  transportation: {
+  carParking: {
     type: Boolean,
     default: false,
   },
+  furnishedRooms: {
+    type: Boolean,
+    default: false,
+  },
+  cctv: {
+    type: Boolean,
+    default: false,
+  },
+  commonAreas: {
+    type: Boolean,
+    default: false,
+  },
+  studyArea: {
+    type: Boolean,
+    default: false,
+  },
+  laundry: {
+    type: Boolean,
+    default: false,
+  },
+  cleaningServices: {
+    type: Boolean,
+    default: false,
+  },
+  internet: {
+    type: Boolean,
+    default: false,
+  },
+  bed: {
+    type: Boolean,
+    default: false,
+  },
+  mattress: {
+    type: Boolean,
+    default: false,
+  },
+  lunch: {
+    type: Boolean,
+    default: false,
+  },
+  dinner: {
+    type: Boolean,
+    default: false,
+  },
+  breakfast: {
+    type: Boolean,
+    default: false,
+  },
+  generator: {
+    type: Boolean,
+    default: false,
+  },
+  ups: {
+    type: Boolean,
+    default: false,
+  },
+  geyser: {
+    type: Boolean,
+    default: false,
+  },
+
   date: {
     type: Date,
     default: Date.now,
