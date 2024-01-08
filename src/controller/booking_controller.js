9541f8250a977172589e26764c7c49201ee7d26c
rashidjatoi <rashidjatoi31@gmail.com>
2024-01-08 15:55:19 +0500

    Booking

---
+++ src/controller/booking_controller.js
@@ -0,0 +1,71 @@
+const BookingModel = require("../models/booking_model");
+const { validationResult } = require("express-validator");
+
+
+
+const bookingController = {
+    // Booking API
+    addBooking: async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            console.log(errors);
+            if (!errors.isEmpty()) {
+                return res
+                    .status(400)
+                    .send({ error: errors.array().map((err) => err.msg[0]) });
+            } else {
+                const { hostelId, hostelname, userId, roomId, bookingDate, checkInDate, checkOutDate, totalAmount, status } = req.body;
+                const existingBooking = await BookingModel.findOne({
+                    hostelId: hostelId,
+                    userId: userId,
+                    roomId: roomId
+                });
+
+                if (existingBooking) {
+                    existingBooking.status = status;
+                    await existingBooking.save();
+                    return res.status(200).send(existingBooking);
+                } else {
+                    const newBooking = await BookingModel.create({
+                        hostelId: hostelId,
+                        hostelname: hostelname,
+                        roomId: roomId,
+                        bookingDate: bookingDate,
+                        userId: userId,
+                        checkInDate: checkInDate,
+                        checkOutDate: checkOutDate,
+                        totalAmount: totalAmount,
+                    });
+                    return res.status(200).send({ message: "Status Changed" });
+                }
+            }
+        } catch (error) {
+            return res.status(500).send({ error: "Internal Server Error", message: error });
+        }
+    },
+
+
+    getBookingsByHostelOrUser: async (req, res) => {
+        try {
+            const { hostelId, userId } = req.body;
+            let bookings;
+            if (hostelId) {
+                bookings = await BookingModel.findOne({ hostelId: hostelId });
+            } else if (userId) {
+                bookings = await BookingModel.find({ userId: userId });
+            }
+
+            return res.status(200).send(bookings);
+
+
+
+        } catch (error) {
+            return res.status(500).send({ error: "Internal Server Error", message: error });
+        }
+    }
+
+
+}
+
+
+module.exports = bookingController;
---
+++ src/models/booking_model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    hostelId: {
+        type: String,
+        // ref: 'hostel',
+        required: true
+    },
+    hostelname: {
+        type: String,
+        // ref: 'hostel',
+        required: true
+    },
+    userId: {
+        type: String,
+        // ref: 'auth',
+        required: true
+    },
+    roomId: {
+        type: String,
+        // ref: 'rooms',
+        required: true
+    },
+    bookingDate: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    checkInDate: {
+        type: Date,
+        required: true
+    },
+    checkOutDate: {
+        type: Date,
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Confirmed', 'Pending', 'Canceled'],
+        default: 'Pending'
+    }
+});
+
+module.exports = mongoose.model('bookings', bookingSchema);
