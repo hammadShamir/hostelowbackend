0d6a311fbc1d42d92d9f8b8c2a47871f89e3520e
Rashid Ali <72204718+rashidjatoi@users.noreply.github.com>
2024-01-16 19:05:46 +0500

    Update room_route.js

--- src/router/room_route.js
+++ src/router/room_route.js
@@ -14,13 +14,7 @@ RoomRouter.post(
     [
         check("hostelId", "Please Enter Hostel ID").not().isEmpty().trim().escape(),
         check("type", "Please Enter Room Type").not().isEmpty().trim().escape(),
-        check("price", "Please Enter Price").not().isEmpty().isNumeric().toInt().trim().escape(),
-        check("beds", "Please Enter beds").not().isEmpty().isNumeric().toInt().trim().escape(),
-        check("description", "Please Enter Room description").not().isEmpty().trim().escape(),
-        check("images", "Please provide valid Images").isArray(),
-        check("amenitities", "Please provide valid Amentities").isArray(),
-        check("availability", "Please enter availability").not().isEmpty().isNumeric().toInt().trim().escape(),
-        check("occupancy", "Please enter occupancy").not().isEmpty().isNumeric().toInt().trim().escape(),
+
     ],
     RoomsController.addOrUpdateRooms
 );
